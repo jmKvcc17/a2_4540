f9e157003e87d2fe661093ab1d0c1561e508ac58
Jesse Meachum <jesse.w.meachum@wmich.edu>
2019-09-18 01:32:11 -0400

    First commit

---
+++ source.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "structs.h"
+
+int main(int argc, char * argv[])
+{
+    process a[48]; 
+	// process id is array index
+	// variables below hold index to process in this array (process id)
+    ui queue[48]; ui queueCount = 0;
+    ui io[48];  ui ioCount = 0;
+    ui cpu;
+
+
+    return 0;
+}
---
+++ structs.h
@@ -0,0 +1,27 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+typedef unsigned int ui;
+struct process_struct {
+    ui priority; // never changes
+    ui cpu;  // time in cpu before I/O
+    ui io; // time I/O takes
+     
+    ui curCpu; // count of current time in CPU
+    ui curIo;  // count of time waiting for I/O
+    ui wait; // current count of time in ready queue
+    ui curPrior; // adjusted for starvation
+    ui cpuTotal; // sum of time in cpu
+    ui ioTotal; // sum of time doing io
+    // statistics
+    ui waitSum; // total time in ready queue
+    ui waitCount; // how many times in ready queue (for average)
+    ui waitMin; // smallest time in ready queue
+    ui waitMax; // longet time in ready queu
+};
+typedef struct process_struct process;
+ 
+struct os_struct { ui quantum; ui wait; };
+typedef struct os_struct os;
+
+#endif
